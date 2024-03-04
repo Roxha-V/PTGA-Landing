e41968cf02049b28f055b2ff0764fd8226c84a25
Roxha-V <rociovalverde1@gmail.com>
2024-03-04 15:59:57 -0300

    feat: :boom: Implementacion Navbar Component   Se crea navbar bar component - Conformado por un div , que contiene 2 columnas, el logo insertado en la columna derecha y luego una estructura de varias columnas donde estan los campos del mismo menu. Esta estilizados con tailwind que deberian funcionar con un diseño responsive en cuanto a variacion de pantallas.
    refactor: :zap: Se refactoriza codigo de navbar-component, donde se busca renderizar una lista de elementos dentro de un array, a los cuales previamente se le asignaron keys. A esto se le suma el mejoramiento de la sitaxis del codigo en cuanto a la aplicacion de estilo de la mano del uso de tailwind y daisy ui.
    
    fix: :art: Se remueve el selector de idiomas , el cual se lograra en otra instancia con un componente propio. A la vez se inserta logo nuevo de PTGA a la navbar
    
    fix: :adhesive_bandage: Se elimina codigo sin uso

---
+++ app/components/navbar-component/index.jsx
@@ -0,0 +1,92 @@
+'use client'
+
+import React from "react";
+
+
+const navbarItems = [
+  { "id": "sobre-ptga", "key": "Sobre PTGA" },
+  { "id": "servicios", "key": "Servicios" },
+  { "id": "clientes", "key": "Clientes" },
+  { "id": "contacto", "key": "Contacto" },
+];
+
+// Extraer componentes con keys. *RECORDAR*: Las keys deben especificarse dentro del array
+//  que se está mapeando (map) y no dentro del componente que se está renderizando. 
+
+
+function NavbarComponent() {
+
+  return (
+    <div className="drawer">
+      <input
+        id="my-drawer-3"
+        type="checkbox"
+        className="drawer-toggle"
+      />
+
+      <div className="drawer-content flex flex-col">
+        <div className="w-full navbar bg-blue-600">
+          <div className="flex-none lg:hidden">
+            <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+
+              <img
+                src="./hamburguericon.svg"
+                alt="hamburguer-icon"
+                className="w-12 h-12 items-center"></img>
+
+            </label>
+          </div>
+
+          <div className="flex-1 px-2 mx-2 ">
+            <a href="/">
+              <img
+                src="./logo-xl-ptga.svg"
+                alt="logo-PTGA"
+                className="w-16 h-16"></img>
+            </a>
+          </div>
+
+
+
+
+          <div className="flex-none hidden lg:block">
+            <ul className="menu menu-horizontal">
+              {navbarItems.map((item, index) => {
+
+                return (
+                  <li key={index} className="navbar-item p-4">
+                    <a href={`#${item.id}`}>{item.key}</a>
+                  </li>
+                );
+
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      {/* aca va el contenido de la pagina entera*/}
+
+
+
+
+
+
+      <div className="drawer-side">  {/* aca arranca la parte del drawer que se muestra en mobile y pantallas chicas ocultando lo anterior */}
+        <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay bg-base-100 rounded-t-none"></label>
+        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+          {navbarItems.map((item, index) => {
+            return (
+              <li key={index} className="navbar-item p-4 ">
+                <a href={`#${item.id}`}>{item.key}</a>
+              </li>
+            );
+
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default NavbarComponent;
