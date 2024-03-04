98fc4c7827b164c9ab23b59650cc1726ab0f1f23
Roxha-V <rociovalverde1@gmail.com>
2024-03-04 15:59:57 -0300

    feat: :boom: Implementacion Navbar Component   Se crea navbar bar component - Conformado por un div , que contiene 2 columnas, el logo insertado en la columna derecha y luego una estructura de varias columnas donde estan los campos del mismo menu. Esta estilizados con tailwind que deberian funcionar con un diseño responsive en cuanto a variacion de pantallas.
    refactor: :zap: Se refactoriza codigo de navbar-component, donde se busca renderizar una lista de elementos dentro de un array, a los cuales previamente se le asignaron keys. A esto se le suma el mejoramiento de la sitaxis del codigo en cuanto a la aplicacion de estilo de la mano del uso de tailwind y daisy ui.
    
    fix: :art: Se remueve el selector de idiomas , el cual se lograra en otra instancia con un componente propio. A la vez se inserta logo nuevo de PTGA a la navbar
    
    fix: :adhesive_bandage: Se elimina codigo sin uso

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,13 @@
 import "./globals.css";
 import Footer from "./components/footer-component";
+import NavbarComponent from "./components/navbar-component";
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <NavbarComponent />
         {children}
         <Footer></Footer>
       </body>
