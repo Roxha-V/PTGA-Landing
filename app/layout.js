5561ec772612543bdf1819c7437c68f5a428f367
Roxha-V <rociovalverde1@gmail.com>
2024-03-04 16:30:26 -0300

    feat: :sparkles: Implementacion hero component
    style: :construction: Añade imagen banner para bg del component
    
    feat: :construction: Construccion de hero - en proceso - solo estructura
    
    style: :lipstick: revision de estilo de parrafo pequeña
    
    feat: :sparkles: Prueba E importacion de fuente para proyecto general
    
    Se importa la fuente en css global
    
    feat: :sparkles: Creacion Slider para hero component
    
    Se implementa la logica para la navegacion de una carrousel de elementos en un array - en este caso banners -
    
     Se implementa la logica para la navegacion de una carrousel de elementos en un array - en este caso banners -
     Se agregan imagenes del array y cambios en el archivo jsx que almacena la funion del componente.
    
    fix: :art: Se agrega funcionalidad de manejadores de carousel y se implementan nuevos tamaños de imagenes para que se puedan adaptar al tamaño de hero. Ala vez tambien se coloca como debe el tamaño del overlay. EL proximo paso es  colocar el color que corresponde al overlay y posicionar correctamente los btns prev/next.
    
    perf: :art: Se mejora el funcionamiento de manejadores, a la vez que se edita el estilo y se define el mismo para el hero/carousel en gral.
    
    fix: :adhesive_bandage: fix path import on Home component

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import "./globals.css";
 import Footer from "./components/footer-component";
 import NavbarComponent from "./components/navbar-component";
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
