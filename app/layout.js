0eecc1601bcb00544edb2595c78fed027e3d5522
Roxha-V <rociovalverde1@gmail.com>
2024-03-22 09:26:06 -0300

    feat: :sparkles: Implementaciòn de componente footer
    se añade componente en la carpeta correspondiente
    se utliza componente en layout
    
    el componente footer utiliza mock de datos e iconos para renderizar redes sociales.

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,12 @@
 import "./globals.css";
+import Footer from "./components/footer-component";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         {children}
+        <Footer></Footer>
       </body>
     </html>
   );
