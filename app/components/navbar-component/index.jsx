2f9d1c097aac971f4fd63aa9ba9a75fedfa39746
Roxha-V <rociovalverde1@gmail.com>
2024-03-25 14:31:09 -0300

    refactor: :zap: Se refactoriza codigo de navbar-component, donde se busca renderizar una lista de elementos dentro de un array, a los cuales previamente se le asignaron keys. A esto se le suma el mejoramiento de la sitaxis del codigo en cuanto a la aplicacion de estilo de la mano del uso de tailwind y daisy ui.

--- app/components/navbar-component/index.jsx
+++ app/components/navbar-component/index.jsx
@@ -1,105 +1,115 @@
+'use client'
+
 import React from "react";
-import logo from "./logo.png";
+// import logo from "app/components/navbar-component/logo.png";
+
+
+const navbarItems = [
+  { "id": "0", "key": "Sobre PTGA" },
+  { "id": "1", "key": "Servicios" },
+  { "id": "2", "key": "Clientes" },
+  { "id": "3", "key": "Contacto" },
+  { "id": "4", "key": "Español" }
+];
+
+// Extraer componentes con keys. *RECORDAR*: Las keys deben especificarse dentro del array
+//  que se está mapeando (map) y no dentro del componente que se está renderizando. 
+
 
 function NavbarComponent() {
+
   return (
     <div className="drawer">
-      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />{" "}
-      {/* aca un checkbox que esta como "oculto" para tooglear de abriero a cerrado */}
+      <input
+        id="my-drawer-3"
+        type="checkbox"
+        className="drawer-toggle"
+      />
+
       <div className="drawer-content flex flex-col">
-        {" "}
-        {/* contenido del navbar */}
-        <div className="navbar-center mx-6 items-center justify-between "></div>
         <div className="w-full navbar bg-blue-600">
           <div className="flex-none lg:hidden">
-            <label
-              htmlFor="my-drawer-3"
-              aria-label="open sidebar"
-              className="btn btn-square btn-ghost"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block w-6 h-6 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
+            <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
             </label>
           </div>
+          <div className="flex-1 px-2 mx-2">PTGA (o logo ptga hay que ver)</div>
+
           <div id="logo" className="flex-1 px-2 mx-2">
             <a href="/">
-              <img className="w-22 h-16" src={logo.src} alt="banner-ptga" />
+              {/* <img className="w-22 h-16" src={logo} alt="banner-ptga" /> */}
             </a>
           </div>
+
           <div className="flex-none hidden lg:block">
             <ul className="menu menu-horizontal">
-              {/* contenido abierto xl lg y md(1) */}
-              <li className="text-white text-2xl">
-                <a href="#">Sobre PTGA</a>
-              </li>
-              <li className="text-white text-2xl">
-                <a href="#">Servicios</a>
-              </li>
-              <li className="text-white text-2xl">
-                <a href="#">Clientes</a>
-              </li>
-              <li className="text-white text-2xl">
-                <a href="#">Contacto</a>
-              </li>
-              <li className="text-white text-2xl">
-                <details>
-                  <summary>Español</summary>
-                  <ul className="rounded-md p-2 bg-blue-600">
-                    <li className="text-white text-2xl">
-                      <a href="#">English</a>
+              {navbarItems.map((item, index) => {
+                if (index === navbarItems.length - 1) {
+                  return (
+                    <li key={index} className="navbar-item mb-4 hover:bg-gray-200">
+                      <details className="text-white text-2xl">
+                        <summary>Español</summary>
+                        <ul className="p-2 bg-base-100 rounded-t-none">
+                          <li>
+                            <a href="#">English</a>
+                          </li>
+
+                        </ul>
+                      </details>
                     </li>
-                  </ul>
-                </details>
-              </li>
+                  );
+                } else {
+                  return (
+                    <li key={index} className="navbar-item p-4 hover:bg-gray-200">
+                      <a href={`#${item.id}`}>{item.key}</a>
+                    </li>
+                  );
+                }
+              })}
             </ul>
           </div>
         </div>
       </div>
-      {/* aca arranca la parte del drawer que se muestra en mobile y pantallas chicas ocultando lo anterior */}
-      <div className="drawer-side">
-        <label
-          htmlFor="my-drawer-3"
-          aria-label="close sidebar"
-          className="drawer-overlay"
-        ></label>
-        <ul className="menu p-4 w-80 min-h-full bg-base-200">
-          {/* contenido xs sm mobile (2)*/}
-          <li className="text-white text-2xl">
-            <a href="#">Sobre PTGA</a>
-          </li>
-          <li className="text-white text-2xl">
-            <a href="#">Servicios</a>
-          </li>
-          <li className="text-white text-2xl">
-            <a href="#">Clientes</a>
-          </li>
-          <li className="text-white text-2xl">
-            <a href="#">Contacto</a>
-          </li>
-          <li className="text-white text-2xl">
-            <details>
-              <summary>Español</summary>
-              <ul className="p-2 bg-base-100 rounded-t-none">
-                <li>
-                  <a href="#">English</a>
+
+      {/* aca va el contenido de la pagina entera*/}
+
+
+
+
+
+
+      <div className="drawer-side">  {/* aca arranca la parte del drawer que se muestra en mobile y pantallas chicas ocultando lo anterior */}
+        <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+          {navbarItems.map((item, index) => {
+
+            if (index === navbarItems.length - 1) {
+
+              return (
+
+                <li key={index} className="navbar-item mb-4 hover:bg-gray-200">
+                  <details className="text-white text-2xl">
+                    <summary>Español</summary>
+                    <ul className="p-2 bg-base-100 rounded-t-none">
+                      <li>
+                        <a href="#">English</a>
+                      </li>
+
+                    </ul>
+                  </details>
                 </li>
-              </ul>
-            </details>
-          </li>
+              );
+            } else {
+              return (
+                <li key={index} className="navbar-item p-4 hover:bg-gray-200">
+                  <a href={`#${item.id}`}>{item.key}</a>
+                </li>
+              );
+            }
+          })}
         </ul>
       </div>
     </div>
   );
 }
+
 export default NavbarComponent;
