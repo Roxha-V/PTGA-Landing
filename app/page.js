bef3f33365e220af1720c5d830a1b7b28c1c39a0
Roxha-V <rociovalverde1@gmail.com>
2024-04-05 11:51:14 -0300

    feat: :tada: Creacion floating-btn-component

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HeroComponent from "./components/hero-component";
 import AboutUs from "./components/about-us-section/index.jsx";
+import FloatingBtn from "./components/Floating-btn-component/FloatingBtn";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <div>
         <HeroComponent />
         <AboutUs />
+        <FloatingBtn />
       </div>
     </main>
   );
