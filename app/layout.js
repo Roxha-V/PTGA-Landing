b38f2696a2dba7b2ddeb84aec687fe472e8de18c
Roxha-V <rociovalverde1@gmail.com>
2024-05-02 11:28:14 -0300

    fix: :adhesive_bandage: Eliminacion de archivo floating-btn-component - archivo vacio que no tenia ni formato.

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,18 @@
 import "./globals.css";
-<<<<<<< HEAD
+
 import Footer from "./components/footer-component";
 import NavbarComponent from "./components/navbar-component";
-=======
->>>>>>> a5e0670 (feat: :sparkles: Creacion Slider para hero component)
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-<<<<<<< HEAD
+
       <body>
         <NavbarComponent />
         {children}
         <Footer></Footer>
       </body>
-=======
-      <body>{children}</body>
->>>>>>> a5e0670 (feat: :sparkles: Creacion Slider para hero component)
+
     </html>
   );
 }
