b93e5b49c2c433efdb282f512a28eb56ce590c47
Roxha-V <rociovalverde1@gmail.com>
2024-05-14 13:50:09 -0300

    fix: :art: *services component fix* - Fixes in : services cards container layout, section background color , card background effect.  it still needs to be done: texts alingment and improve elements responsiveness.

--- app/components/services-section/index.jsx
+++ app/components/services-section/index.jsx
@@ -7,8 +7,8 @@ const cardData = [
         id: 0, title: "Instalaciones", icon: "", img: "", bgColor: "#FEA700", service_detail:
             "Instalación y mantenimiento y cambio de: Torres de telecomunicaciones. Equipos y enlaces en altura. Equipos de estudio. Transmisores", link: ""
     },
-    { id: 1, title: "Mantenimiento de plantas trasmisoras FM", icon: "", img: "", bgColor: "#A30DFC", service_detail: "Nuestros técnicos realizan las tareas de mantenimiento de punta a punta: desde los equipos del estudio hasta la baliza de la torre, garantizando el buen funcionamiento de tu planta transmisora, realizando tareas como:  Cambio de antenas y enlaces. Mantenimiento de torres. Asesoría para la compra equipos de estudio y planta transmisora.    ", link: "?" },
-    { id: 2, title: "Desmonte", icon: "", img: "", bgColor: "#2A00BF", service_detail: "El trabajo profesional de nuestras cuadrillas de técnicos, con capacitación en seguridad laboral para el trabajo en altura, resuelven situaciones de potencial peligro para un inmueble.", link: "?" },
+    { id: 2, title: "Mantenimiento de plantas trasmisoras FM", icon: "", img: "", bgColor: "#A30DFC", service_detail: "Nuestros técnicos realizan las tareas de mantenimiento de punta a punta: desde los equipos del estudio hasta la baliza de la torre, garantizando el buen funcionamiento de tu planta transmisora, realizando tareas como:  Cambio de antenas y enlaces. Mantenimiento de torres. Asesoría para la compra equipos de estudio y planta transmisora.    ", link: "?" },
+    { id: 1, title: "Desmonte", icon: "", img: "", bgColor: "#2A00BF", service_detail: "El trabajo profesional de nuestras cuadrillas de técnicos, con capacitación en seguridad laboral para el trabajo en altura, resuelven situaciones de potencial peligro para un inmueble.", link: "?" },
     { id: 3, title: "Eficiencia energética", icon: "", img: "", bgColor: "#ED0000", service_detail: "Nuestra misión es asesorar, reformar, re acondicionar instalaciones energéticas con el enfoque en ahorro y efectividad en el uso que se le de en cada establecimiento. Analizamos las demandas y las necesidades del cliente a fin de realizar el proyecto y buscar las oportunidades de ahorro en las tarifas de electricidad y gas. Nos manejamos en las áreas domiciliarias, comerciales e industriales.", link: "?" },
     //#5C0092 violeta oscuro
 
@@ -16,8 +16,23 @@ const cardData = [
 
 function ServiceSection(params) {
 
-    return <div id="serviviios">
-        {cardData.map((item) => <Card {...item} key={item.id} />)}
-    </div>
+    return (
+        <div id="services">
+            <div>
+                <div className="relative flex justify-center overflow-hidden bg-gray-100 p-6 sm:py-12 
+             lg:flex-row items-stretch flex-wrap gap-10 md:flex-col sm:flex-col">
+                    {cardData.map((item) => <Card {...item} key={item.id} />)}
+                </div>
+            </div>
+        </div>
+    )
 }
 export default ServiceSection
+
+
+// Aque lo que hay es un componente que renderiza una lista de componentes con nombre <Card>.
+//Cada <Card> recibe props (propiedades) basados en los elementos item dentro de los objetos que forman parte del array cardData,
+//donde cada uno tiene una key única basada en su id.
+
+
+//{...item} en el contexto de pasar props a un componente en react, como en <Card {...item} key={item.id} />, significa que todas las propiedades del objeto item se están pasando como props individuales al componente <Card>.
--- app/components/services-section/services-card/index.jsx
+++ app/components/services-section/services-card/index.jsx
@@ -17,33 +17,37 @@ function Card({ id, title, img, bgColor, service_detail }) {
     // aca se entiende que isOpen es la card cardAbierta pero que en su estado inicial es false (osea que no esta isOpen),
     // mientras que si se usa el operador ! este estado cambia, osea es el opuesto
 
-    return (
-        <div className="relative flex m flex-col justify-center overflow-hidden bg-orange-200 py-6 sm:py-12">
-            <div className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
-                <span className="absolute top-10 z-0 h-20 w-20 rounded-full transition-all duration-300 group-hover:scale-[10]" style={{ backgroundColor: bgColor }}></span>
-                <div className="relative z-10 mx-auto max-w-md">
-                    <span className="grid h-20 w-20 place-items-center rounded-full transition-all duration-200 group-hover:bg-[var(--hover)] "
-                        style={{
-                            '--hover': bgColor
-                        }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-10 w-10 text-white transition-all">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-                        </svg>
-                    </span>
-                    <div className="pt-5 text-base font-semibold leading-7">
-                        <p>
-                            <a href="#" className="text-amber-500 transition-all duration-300 group-hover:text-white/90">{title}
-                            </a>
-                        </p>
-                    </div>
-                    <div
-                        className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
-                        <p>{service_detail}</p>
-                    </div>
 
+    return (
+
+
+
+        <div className="group relative cursor-pointer overflow-hidden bg-white pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto rounded-lg  px-6 sm:px-10">
+            <span className="absolute top-10 z-0 h-20 w-20 rounded-full transition-all duration-300 group-hover:scale-[15]" style={{ backgroundColor: bgColor }}></span>
+            <div className="relative z-10 mx-auto max-w-md">
+                <span className="grid h-20 w-20 place-items-center rounded-full transition-all duration-200 group-hover:bg-[var(--hover)] "
+                    style={{
+                        '--hover': bgColor
+                    }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-10 w-10 text-white transition-all">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+                    </svg>
+                </span>
+                <div className="pt-5 text-base font-semibold leading-7">
+                    <p>
+                        <a href="#" className={`text-${bgColor} transition-all duration-300 group-hover:text-white/90`}>{title}
+                        </a>
+
+                    </p>
                 </div>
+                <div
+                    className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
+                    <p>{service_detail}</p>
+                </div>
+
             </div>
-        </div>
+        </div >
+
     );
 }
 
