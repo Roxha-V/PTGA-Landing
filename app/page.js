b4c41bfa3be798b28e08ed21b3eb8dd1c5079ca9
Roxha-V <rociovalverde1@gmail.com>
2024-05-29 17:23:25 -0300

    feat: :sparkles: Implementation of contact-section structure - (only structure)

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import HeroComponent from "./components/hero-component";
 import AboutUs from "./components/about-us-section/index.jsx";
 import FloatingBtn from "./components/Floating-btn-component/FloatingBtn";
 import Services from "./components/services-section";
+import ContactComponent from "./components/contact_section";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <HeroComponent />
       <AboutUs />
       <Services />
+      <ContactComponent />
       <FloatingBtn />
     </>
   );
