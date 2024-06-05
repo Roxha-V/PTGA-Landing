89c628b226efdddaea0584aa50c362bfda7d9d42
Roxha-V <rociovalverde1@gmail.com>
2024-06-05 12:45:04 -0300

    fix: :lipstick: 36-Fix cards style

--- app/components/services-section/services-card/index.jsx
+++ app/components/services-section/services-card/index.jsx
@@ -6,11 +6,11 @@ function Card({ id, title, img, bgColor, service_detail }) {
   return (
     <div
       id="touch-card"
-      className="group relative cursor-pointer overflow-hidden bg-white pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto rounded-lg  px-6 sm:px-10"
+      className="group relative cursor-pointer overflow-hidden bg-white pt-10 pb-8  ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto rounded-lg  px-6 sm:px-10"
     >
       <span
         id="card-span"
-        className="absolute top-10 z-0 h-20 w-20 rounded-full transition-all duration-300 group-hover:scale-[15] "
+        className="absolute top-10 z-0 h-20 w-20 rounded-full transition-all duration-300 group-hover:scale-[25] "
         style={{ backgroundColor: bgColor }}
       ></span>
       <div className="relative z-10 mx-auto max-w-md">
@@ -34,7 +34,7 @@ function Card({ id, title, img, bgColor, service_detail }) {
         </div>
         <div
           id="card-parag"
-          className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300  text-left text-balance pr-24 group-hover:text-white/90"
+          className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300  text-left text-balance group-hover:text-white/90"
         >
           <p>{service_detail}</p>
         </div>
