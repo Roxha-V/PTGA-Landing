905d883658838639412f8a9da173e9bc63257209
Roxha-V <rociovalverde1@gmail.com>
2024-06-10 19:59:47 -0300

    style: :lipstick: upgrade about us section  -  - Matched background color with previous section - Added a card-style wrapper for improved content presentation.

--- app/components/about-us-section/index.jsx
+++ app/components/about-us-section/index.jsx
@@ -1,28 +1,45 @@
-'use client';
+"use client";
 
 import React, { useState } from "react";
-import { Montserrat } from 'next/font/google';
+import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({
-    weight: '400',
-    subsets: ['latin'],
-})
+  weight: "400",
+  subsets: ["latin"],
+});
 
 function AboutUs() {
+  return (
+    <>
+      <div id="about-us-section">
+        <div className="w-full p-4 bg-gray-100">
+          <div className="w-[100%]shadow-xl bg-indigo-800 py-10 px-5 rounded-md ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl mx-auto">
+            <div className="flex flex-col">
+              <div className="flex flex-col items-start">
+                <h1 className=" ml-10 lg:text-6xl md:text-4xl sm:text-2xl text-pretty uppercase  text-left text-white mb-2">
+                  Sobre PTGA
+                </h1>
 
-    return (
-        <>
-            <div id="about-us-section" className="w-full block py-10 bg-cyan-900 justify-center">
-                <h1 className="font-sans ceraprobold p-10 mb-2 text-5xl text-pretty uppercase leading-8 tracking-widest font-bold text-center text-white">Sobre PTGA</h1>
-                <p className={`${montserrat.className} object-contain font-sans px-40 md:text-[1.25rem] md:leading-normal lg:text-[2.25rem] lg:leading-loose text-white text-justify`}>
-                    Somos una cooperativa  especializada en asesoramiento, investigación, fabricación y servicios de instalación para el desarrollo de tecnología en telecomunicaciones.
-                    Contamos con más de una década de experiencia profesional en el campo, personal técnico especializado y la infraestructura necesaria para brindar servicios integrales en materia de comunicación audiovisual.
-                    Nuestra vasta experiencia profesional nos permite ofrecer un conjunto de soluciones diseñadas que se ajustan a cada proyecto. </p>
+                <p
+                  className={`${montserrat.className} md:text-[1rem] md:leading-normal lg:text-[2rem] text-white  text-justify p-4`}
+                >
+                  Somos una cooperativa especializada en asesoramiento,
+                  investigación, fabricación y servicios de instalación para el
+                  desarrollo de tecnología en telecomunicaciones. Contamos con
+                  más de una década de experiencia profesional en el campo,
+                  personal técnico especializado y la infraestructura necesaria
+                  para brindar servicios integrales en materia de comunicación
+                  audiovisual. Nuestra vasta experiencia profesional nos permite
+                  ofrecer un conjunto de soluciones diseñadas que se ajustan a
+                  cada proyecto.
+                </p>
+              </div>
             </div>
-
-        </>
-
-    )
-};
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default AboutUs;
