20249fcc5c52ea085799f8ae39e91406cf17b6cb
Roxha-V <rociovalverde1@gmail.com>
2024-06-11 19:38:29 -0300

    style: :lipstick: test cambios generales ui styles

--- app/components/about-us-section/index.jsx
+++ app/components/about-us-section/index.jsx
@@ -10,60 +10,44 @@ const montserrat = Montserrat({
 
 function AboutUs() {
   const sectionStyle = {
-    backgroundImage: "linear-gradient(-45deg,#120320,#43005F, #5C0092)",
+    backgroundImage: "linear-gradient(-25deg,#120320,#43005F, #5C0092)",
   };
 
   return (
     <>
       <div id="about-us-section">
-        <div className="w-full p-4 bg-gray-100">
+        <div className="w-full p-4">
           <div
             style={sectionStyle}
-            className="w-full shadow-xl py-10 px-5 rounded-md ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl mx-auto"
+            className="grid grid-cols-2 gap-4 w-full shadow-xl py-10 px-5 rounded-md ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl mx-auto"
           >
-            <style>{`
-          @keyframes gradient {
-            0% {
-              background-position: 0% 50%;
-            }
-            50% {
-              background-position: 100% 50%;
-            }
-            100% {
-              background-position: 0% 50%;
-            }
-          }
-        `}</style>
             <div className="flex flex-col">
               <div
-                className="absolute bottom-0 left-0 w-full h-full bg-no-repeat"
-                style={{
-                  backgroundImage: "url(public/torrre png.png') ",
-                  backgroundSize: "384px",
-                  backgroundPositionX: "0px",
-                  backgroundPositionY: "0px",
-                }}
-              ></div>
-              <div className="flex flex-col items-start">
-                <h1 className="ml-10 lg:text-6xl md:text-4xl sm:text-2xl text-pretty uppercase text-left text-white mb-2">
-                  Sobre PTGA
-                </h1>
+                className="relative 
+              "
+              >
+                <div className="flex flex-col items-start">
+                  <h1 className="ml-10 lg:text-5xl md:text-3xl sm:text-2xl text-pretty uppercase text-left  mb-2">
+                    Sobre PTGA
+                  </h1>
 
-                <p
-                  className={`${montserrat.className} md:text-[1rem] md:leading-normal lg:text-[2rem] text-white text-justify p-4`}
-                >
-                  Somos una cooperativa especializada en asesoramiento,
-                  investigación, fabricación y servicios de instalación para el
-                  desarrollo de tecnología en telecomunicaciones. Contamos con
-                  más de una década de experiencia profesional en el campo,
-                  personal técnico especializado y la infraestructura necesaria
-                  para brindar servicios integrales en materia de comunicación
-                  audiovisual. Nuestra vasta experiencia profesional nos permite
-                  ofrecer un conjunto de soluciones diseñadas que se ajustan a
-                  cada proyecto.
-                </p>
+                  <p
+                    className={`${montserrat.className} md:text-[1rem] md:leading-normal lg:text-[1.5rem] text-white text-justify p-4`}
+                  >
+                    Somos una cooperativa especializada en asesoramiento,
+                    investigación, fabricación y servicios de instalación para
+                    el desarrollo de tecnología en telecomunicaciones. Contamos
+                    con más de una década de experiencia profesional en el
+                    campo, personal técnico especializado y la infraestructura
+                    necesaria para brindar servicios integrales en materia de
+                    comunicación audiovisual. Nuestra vasta experiencia
+                    profesional nos permite ofrecer un conjunto de soluciones
+                    diseñadas que se ajustan a cada proyecto.
+                  </p>
+                </div>
               </div>
             </div>
+            <div style={{ backgroundImage: "url(public/torrepng.png)" }}></div>
           </div>
         </div>
       </div>
