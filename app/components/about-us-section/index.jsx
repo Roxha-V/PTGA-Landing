518bde00bed4069118d607d0d839a1efd2dc35f5
Roxha-V <rociovalverde1@gmail.com>
2024-06-18 15:03:25 -0300

    55-mobile- fix: about us section:adhesive_bandage:

--- app/components/about-us-section/index.jsx
+++ app/components/about-us-section/index.jsx
@@ -1,27 +1,27 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import { Montserrat } from 'next/font/google';
+import React, { useState } from "react";
+import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({
-  weight: '400',
-  subsets: ['latin'],
+  weight: "400",
+  subsets: ["latin"],
 });
 
 function AboutUs() {
   return (
     <>
       <div id="about-us-section">
-        <div className="w-full p-24 bg-gray-100 ">
-          <div className="w-[100%] bg-white py-5 px-5 rounded-md  transition-all duration-300 ">
-            <div className="flex flex-col">
-              <div className="flex flex-col items-start">
-                <div className="flex items-start tracking-tighter	">
-                  <h1 className=" lg:text-6xl md:text-4xl sm:text-2xl text-pretty uppercase  text-left text-slate-700 mb-2">
-                    ¿Quienes{' '}
+        <div className="w-full p-8 md:p-6 lg:p-10 bg-gray-100 ">
+          <div className="w-[100%] bg-white  md:py-5 md:px-5 rounded-md">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mr-4">
+              <div className="completo-texto flex flex-col items-start w-full align-middle">
+                <div className="solo-titulo flex flex-row pl-5 pt-4 mb-2 text-center items-center">
+                  <h1 className=" lg:text-6xl md:text-4xl text-2xl text-pretty uppercase  text-left text-slate-700 mb-2">
+                    ¿Quienes{" "}
                   </h1>
-                  <h1 className=" pl-5 lg:text-6xl md:text-4xl sm:text-2xl text-pretty uppercase  text-left text-indigo-600 mb-2">
-                    {' '}
+                  <h1 className=" pl-5 lg:text-6xl md:text-4xl text-2xl text-pretty uppercase  text-left text-indigo-600 mb-2">
+                    {" "}
                     somos?
                   </h1>
                 </div>
@@ -29,29 +29,29 @@ function AboutUs() {
                 <p
                   className={`${montserrat.className} md:text-[1rem] leading-normal lg:text-[1.3rem] text-slate-700  text-justify p-4`}
                 >
-                  Somos una{' '}
+                  Somos una{" "}
                   <span className="text-indigo-600">
                     cooperativa especializada
-                  </span>{' '}
+                  </span>{" "}
                   en asesoramiento, investigación, fabricación y servicios de
                   instalación para el desarrollo de tecnología
                   <span className="text-indigo-600">
-                    {' '}
+                    {" "}
                     en telecomunicaciones
                   </span>
-                  .<br></br> Contamos con más de{' '}
+                  .<br></br> Contamos con más de{" "}
                   <span className="text-amber-600">
                     una década de experiencia profesional
-                  </span>{' '}
+                  </span>{" "}
                   en el campo, personal técnico especializado y la
                   <span className="text-amber-600">
-                    {' '}
+                    {" "}
                     infraestructura necesaria para brindar servicios integrales
-                  </span>{' '}
+                  </span>{" "}
                   en materia de comunicación audiovisual.<br></br> Nuestra vasta
                   experiencia profesional nos permite ofrecer un conjunto de
                   <span className="text-red-500">
-                    {' '}
+                    {" "}
                     soluciones diseñadas que se ajustan a cada proyecto.
                   </span>
                 </p>
--- app/components/services-section/services-card/index.jsx
+++ app/components/services-section/services-card/index.jsx
@@ -6,14 +6,14 @@ function Card({ id, title, img, bgColor, service_detail }) {
   return (
     <div
       id="touch-card"
-      className="group relative cursor-pointer overflow-hidden bg-white pt-10 pb-8  ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto rounded-lg  px-6 sm:px-10"
+      className="group w-full relative cursor-pointer overflow-hidden bg-white pt-10 pb-8  ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto rounded-lg  px-6 sm:px-10"
     >
       <span
         id="card-span"
         className="absolute top-10 z-0 h-20 w-20 rounded-full transition-all duration-300 group-hover:scale-[25] "
         style={{ backgroundColor: bgColor }}
       ></span>
-      <div className="relative z-10 mx-auto max-w-md">
+      <div className="relative z-10 ">
         <span
           className="grid h-20 w-20 place-items-center rounded-full transition-all duration-200 group-hover:bg-[var(--hover)] "
           style={{
@@ -36,7 +36,7 @@ function Card({ id, title, img, bgColor, service_detail }) {
           id="card-parag"
           className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300  text-left text-balance group-hover:text-white/90"
         >
-          <p>{service_detail}</p>
+          {service_detail}
         </div>
       </div>
     </div>
