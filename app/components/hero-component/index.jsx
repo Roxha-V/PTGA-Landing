f0b36c7e84a344261f229493b464bdfa68928b60
Roxha-V <rociovalverde1@gmail.com>
2024-07-03 17:47:46 -0300

    resolve #78 add SEO elements

--- app/components/hero-component/index.jsx
+++ app/components/hero-component/index.jsx
@@ -3,20 +3,22 @@ import React from "react";
 function HeroComponent() {
   return (
     <>
-      <div
-        className="hero min-h-[100dvh]"
-        style={{ backgroundImage: "url(/img/bannerhero4.jpg)" }}
-      >
-        <div className="hero-overlay bg-opacity-60"></div>
+      <section aria-label="Hero Section">
+        <div
+          className="hero min-h-[100dvh]"
+          style={{ backgroundImage: "url(/img/bannerhero4.jpg)" }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
 
-        <div className="hero-content text-neutral-content">
-          <div className="w-9/12 pt-10 ">
-            <h1 className="box-content mb-5 text-center lg:text-left text-4xl md:text-5xl lg:text-7xl font-bold">
-              Producción Tecnológica, Gráfica <br />y Audiovisual LTDA
-            </h1>
+          <div className="hero-content text-neutral-content">
+            <div className="w-9/12 pt-10 ">
+              <h1 className="box-content mb-5 text-center lg:text-left text-4xl md:text-5xl lg:text-7xl font-bold">
+                Producción Tecnológica, Gráfica <br />y Audiovisual LTDA
+              </h1>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
